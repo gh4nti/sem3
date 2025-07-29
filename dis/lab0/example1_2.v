0161f802447be672ba5b8a57db77fa666a8ccfbc
Adarsh Inaganti <adarshinaganti006@gmail.com>
2025-07-29 23:31:56 +0530

    added structural code

---
+++ dis/lab0/example1_2.v
@@ -0,0 +1,10 @@
+module example1(x1, x2, x3, f);
+    input x1, x2, x3;
+    output f;
+    wire w1, w2, nx2;
+
+    not (nx2, x2);
+    and (w1, x1, x2);
+    and (w2, nx2, x3);
+    or (f, w1, w2);
+endmodule
