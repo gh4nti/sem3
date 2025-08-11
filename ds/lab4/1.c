9d9169072f0ad2f0c670e99073fa459422230155
adarsh <93140946+gh4nti@users.noreply.github.com>
2025-08-11 15:09:14 +0530

    added question comment

--- ds/lab4/1.c
+++ ds/lab4/1.c
@@ -1,3 +1,22 @@
+/*
+Write a menu-driven C program using structures to implement the following operations on a singly linked list:
+- Insert an element before another specified element in the list
+(Example: Insert 10 before 25)
+- Insert an element after another specified element in the list
+(Example: Insert 40 after 25)
+- Delete a specified element from the list
+(Example: Delete node containing 15)
+- Traverse the list and display all elements
+- Reverse the linked list
+(Modify the links such that the list is reversed)
+- Sort the list in ascending order
+(Using Bubble Sort or any appropriate algorithm on linked list)
+- Delete every alternate node in the list
+(Starting from the second node)
+- Insert an element into a sorted linked list while maintaining the sorted order
+(Example: Insert 28 into a list that is already sorted)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
