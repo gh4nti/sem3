e2fba0277c2bebf6aac7955fe8fdbcdcdc4a1af5
adarsh <93140946+gh4nti@users.noreply.github.com>
2025-08-11 15:52:15 +0530

    changed approach

--- ds/lab3/1.c
+++ ds/lab3/1.c
@@ -20,7 +20,7 @@ void strDelete(char *str, int pos, int len);
 int main()
 {
 	char *str1, *str2, *res, *sub;
-	int c, pos, len;
+	int pos, len;
 
 	str1 = (char *)malloc(MAX * sizeof(char));
 	str2 = (char *)malloc(MAX * sizeof(char));
@@ -35,101 +35,78 @@ int main()
 
 	printf("Enter the first string: ");
 	fgets(str1, MAX, stdin);
+
 	printf("Enter the second string: ");
 	fgets(str2, MAX, stdin);
 
 	// remove trailing newline character, if any
-	if (str1[strLength(str1) - 1] == '\n')
-		str1[strLength(str1) - 1] = '\0';
-	if (str2[strLength(str2) - 1] == '\n')
-		str2[strLength(str2) - 1] = '\0';
+	if (str1[stringLength(str1) - 1] == '\n')
+		str1[stringLength(str1) - 1] = '\0';
+	if (str2[stringLength(str2) - 1] == '\n')
+		str2[stringLength(str2) - 1] = '\0';
 
-	while (1)
+	printf("\nString Operations:\n");
+
+	// Length
+	printf("Length of the first string: %d\n", stringLength(str1));
+	printf("Length of the second string: %d\n", stringLength(str2));
+
+	// Concatenation
+	stringConcat(res, str1, str2);
+	printf("Concatenated string: %s\n", res);
+
+	// Comparison
+	if (stringCompare(str1, str2))
 	{
-		printf("\nChoose an operation:\n");
-		printf("1. Length of the string\n");
-		printf("2. Concatenate two strings\n");
-		printf("3. Compare two strings\n");
-		printf("4. Insert a substring\n");
-		printf("5. Delete a substring\n");
-		printf("6. Exit\n");
-		printf("Enter your choice: ");
-		scanf("%d", &c);
-		getchar();
-
-		switch (c)
-		{
-		case 1:
-			printf("Length of the first string: %d\n", strLength(str1));
-			printf("Length of the second string: %d\n", strLength(str2));
-			break;
-
-		case 2:
-			strConcat(res, str1, str2);
-			printf("Concatenated string: %s\n", res);
-			break;
-
-		case 3:
-			if (strCompare(str1, str2))
-			{
-				printf("The strings are equal.\n");
-			}
-			else
-			{
-				printf("The strings are not equal.\n");
-			}
-			break;
-
-		case 4:
-			printf("Enter the position to insert the substring: ");
-			scanf("%d", &pos);
-			getchar(); // to clear the newline
-
-			printf("Enter the substring to insert: ");
-			fgets(sub, MAX, stdin);
-			sub[strLength(sub) - 1] = '\0'; // Remove the newline character
-
-			if (pos >= 0 && pos <= strLength(str1))
-			{
-				strInsert(str1, sub, pos);
-				printf("String after insertion: %s\n", str1);
-			}
-			else
-			{
-				printf("Invalid position for insertion.\n");
-			}
-			break;
-
-		case 5:
-			printf("Enter the position to delete the substring: ");
-			scanf("%d", &pos);
-			printf("Enter the length of the substring to delete: ");
-			scanf("%d", &len);
-
-			if (pos >= 0 && pos < strLength(str1) && len >= 0 && pos + len <= strLength(str1))
-			{
-				strDelete(str1, pos, len);
-				printf("String after deletion: %s\n", str1);
-			}
-			else
-			{
-				printf("Invalid position or length for deletion.\n");
-			}
-			break;
-
-		case 6:
-			printf("Exiting...\n");
-			free(str1);
-			free(str2);
-			free(res);
-			free(sub);
-			return 0;
-
-		default:
-			printf("Invalid choice! Please try again.\n");
-		}
+		printf("The strings are equal.\n");
 	}
 
+	else
+	{
+		printf("The strings are not equal.\n");
+	}
+
+	// Insertion
+	printf("Enter the position to insert the substring: ");
+	scanf("%d", &pos);
+	getchar(); // to clear the newline
+
+	printf("Enter the substring to insert: ");
+	fgets(sub, MAX, stdin);
+	sub[stringLength(sub) - 1] = '\0'; // Remove the newline character
+
+	if (pos >= 0 && pos <= stringLength(str1))
+	{
+		stringInsert(str1, sub, pos);
+		printf("String after insertion: %s\n", str1);
+	}
+
+	else
+	{
+		printf("Invalid position for insertion.\n");
+	}
+
+	// Deletion
+	printf("Enter the position to delete the substring: ");
+	scanf("%d", &pos);
+	printf("Enter the length of the substring to delete: ");
+	scanf("%d", &len);
+
+	if (pos >= 0 && pos < stringLength(str1) && len >= 0 && pos + len <= stringLength(str1))
+	{
+		stringDelete(str1, pos, len);
+		printf("String after deletion: %s\n", str1);
+	}
+
+	else
+	{
+		printf("Invalid position or length for deletion.\n");
+	}
+
+	free(str1);
+	free(str2);
+	free(res);
+	free(sub);
 	return 0;
 }
 
