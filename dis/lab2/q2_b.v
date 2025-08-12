160ca238134d9521417249953e85d58c71665f7d
Adarsh Inaganti <adarshinaganti006@gmail.com>
2025-08-12 19:56:49 +0530

    fixed expression

--- dis/lab2/q2_b.v
+++ dis/lab2/q2_b.v
@@ -2,6 +2,6 @@ module q2_b(a, b, c, d, f);
 	input a, b, c, d;
 	output f;
 
-	assign f = (~b | c | d) & (~a | b | ~d) & (a | ~c | d) & (b | ~c | d);
+	assign f = (~a | b | ~d) & (~b | c | d) & (a | ~b | d) & (~a | b | ~c);
 
 endmodule
