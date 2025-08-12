c83dce5a04949d3e59be8f504f75b637c9e57ee0
Adarsh Inaganti <adarshinaganti006@gmail.com>
2025-08-12 15:12:20 +0530

    fixed typo

--- dis/lab2/q1_a.v
+++ dis/lab2/q1_a.v
@@ -2,6 +2,6 @@ module q1_a(a, b, c, d, f);
 	input a, b, c, d;
 	output f;
 
-	assign f = (~a & b) | (~b & c) | (c & d) | (b & c & ~d);
+	assign f = (~a & b) | (~b & c) | (c & d) | (b & ~c & ~d);
 
 endmodule
