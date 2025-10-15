8eb2ac9ebaa4a8829b02c4915ce65e9f91e5f656
adarsh <adarshinaganti006@gmail.com>
2025-10-15 15:59:37 +0530

    added additional problems

---
+++ ds/lab11/a2.c
@@ -0,0 +1,128 @@
+// Detect a cycle in a directed graph using recursion stack.
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+
+typedef struct Node
+{
+	int vertex;
+	struct Node *next;
+} Node;
+
+typedef struct Graph
+{
+	int vertices;
+	Node *adjLists[MAX];
+} Graph;
+
+int visited[MAX], stack[MAX];
+
+Node *createNode(int v);
+Graph *createGraph(int vertices);
+void addEdge(Graph *graph, int src, int dest);
+int DFS_Cycle(Graph *graph, int vertex);
+int hasCycle(Graph *graph);
+
+int main()
+{
+	int vertices, edges, src, dest;
+
+	printf("Enter number of vertices: ");
+	scanf("%d", &vertices);
+
+	Graph *graph = createGraph(vertices);
+
+	printf("Enter number of edges: ");
+	scanf("%d", &edges);
+
+	printf("Enter edges (source destination) for directed graph:\n");
+	for (int i = 0; i < edges; i++)
+	{
+		scanf("%d %d", &src, &dest);
+		addEdge(graph, src, dest);
+	}
+
+	if (hasCycle(graph))
+		printf("Graph contains a cycle.\n");
+	else
+		printf("Graph does not contain a cycle.\n");
+
+	for (int i = 0; i < vertices; i++)
+	{
+		Node *temp = graph->adjLists[i];
+		while (temp)
+		{
+			Node *toFree = temp;
+			temp = temp->next;
+			free(toFree);
+		}
+	}
+	free(graph);
+
+	return 0;
+}
+
+Node *createNode(int v)
+{
+	Node *newNode = (Node *)malloc(sizeof(Node));
+	newNode->vertex = v;
+	newNode->next = NULL;
+	return newNode;
+}
+
+Graph *createGraph(int vertices)
+{
+	Graph *graph = (Graph *)malloc(sizeof(Graph));
+	graph->vertices = vertices;
+	for (int i = 0; i < vertices; i++)
+		graph->adjLists[i] = NULL;
+	return graph;
+}
+
+void addEdge(Graph *graph, int src, int dest)
+{
+	Node *newNode = createNode(dest);
+	newNode->next = graph->adjLists[src];
+	graph->adjLists[src] = newNode;
+}
+
+int DFS_Cycle(Graph *graph, int vertex)
+{
+	if (!visited[vertex])
+	{
+		visited[vertex] = 1;
+		stack[vertex] = 1;
+
+		Node *temp = graph->adjLists[vertex];
+		while (temp)
+		{
+			int adjVertex = temp->vertex;
+			if (!visited[adjVertex] && DFS_Cycle(graph, adjVertex))
+				return 1;
+			else if (stack[adjVertex])
+				return 1;
+			temp = temp->next;
+		}
+	}
+
+	stack[vertex] = 0;
+	return 0;
+}
+
+int hasCycle(Graph *graph)
+{
+	for (int i = 0; i < graph->vertices; i++)
+	{
+		visited[i] = 0;
+		stack[i] = 0;
+	}
+
+	for (int i = 0; i < graph->vertices; i++)
+	{
+		if (DFS_Cycle(graph, i))
+			return 1;
+	}
+
+	return 0;
+}
---
+++ ds/lab11/a3.c
@@ -0,0 +1,193 @@
+// Check if a graph is bipartite using BFS.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+#define UNCOLORED -1
+
+typedef struct Node
+{
+	int vertex;
+	struct Node *next;
+} Node;
+
+typedef struct Graph
+{
+	int numVertices;
+	Node *adjLists[MAX];
+} Graph;
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+int isEmpty();
+void enqueue(int value);
+int dequeue();
+Node *createNode(int v);
+Graph *createGraph(int vertices);
+void addEdge(Graph *graph, int src, int dest);
+int isBipartite(Graph *graph, int start);
+int checkBipartite(Graph *graph);
+
+int main()
+{
+	int vertices, edges, src, dest;
+
+	printf("Enter number of vertices: ");
+	scanf("%d", &vertices);
+
+	Graph *graph = createGraph(vertices);
+
+	printf("Enter number of edges: ");
+	scanf("%d", &edges);
+
+	printf("Enter edges (source destination):\n");
+	for (int i = 0; i < edges; i++)
+	{
+		scanf("%d %d", &src, &dest);
+		addEdge(graph, src, dest);
+	}
+
+	if (checkBipartite(graph))
+		printf("Graph is bipartite.\n");
+	else
+		printf("Graph is not bipartite.\n");
+
+	for (int i = 0; i < vertices; i++)
+	{
+		Node *temp = graph->adjLists[i];
+		while (temp)
+		{
+			Node *toFree = temp;
+			temp = temp->next;
+			free(toFree);
+		}
+	}
+	free(graph);
+
+	return 0;
+}
+
+int isEmpty()
+{
+	return front == -1;
+}
+
+void enqueue(int value)
+{
+	if (rear == MAX - 1)
+		return;
+	if (isEmpty())
+		front = 0;
+	queue[++rear] = value;
+}
+
+int dequeue()
+{
+	if (isEmpty())
+		return -1;
+	int item = queue[front];
+	if (front == rear)
+		front = rear = -1;
+	else
+		front++;
+	return item;
+}
+
+Node *createNode(int v)
+{
+	Node *newNode = (Node *)malloc(sizeof(Node));
+	newNode->vertex = v;
+	newNode->next = NULL;
+	return newNode;
+}
+
+Graph *createGraph(int vertices)
+{
+	Graph *graph = (Graph *)malloc(sizeof(Graph));
+	graph->numVertices = vertices;
+	for (int i = 0; i < vertices; i++)
+		graph->adjLists[i] = NULL;
+	return graph;
+}
+
+void addEdge(Graph *graph, int src, int dest)
+{
+	Node *newNode = createNode(dest);
+	newNode->next = graph->adjLists[src];
+	graph->adjLists[src] = newNode;
+
+	newNode = createNode(src);
+	newNode->next = graph->adjLists[dest];
+	graph->adjLists[dest] = newNode;
+}
+
+int isBipartite(Graph *graph, int start)
+{
+	int color[MAX];
+	for (int i = 0; i < graph->numVertices; i++)
+		color[i] = UNCOLORED;
+
+	color[start] = 0;
+	enqueue(start);
+
+	while (!isEmpty())
+	{
+		int u = dequeue();
+		Node *temp = graph->adjLists[u];
+		while (temp)
+		{
+			int v = temp->vertex;
+			if (color[v] == UNCOLORED)
+			{
+				color[v] = 1 - color[u];
+				enqueue(v);
+			}
+			else if (color[v] == color[u])
+			{
+				return 0;
+			}
+			temp = temp->next;
+		}
+	}
+	return 1;
+}
+
+int checkBipartite(Graph *graph)
+{
+	int color[MAX];
+	for (int i = 0; i < graph->numVertices; i++)
+		color[i] = UNCOLORED;
+
+	for (int i = 0; i < graph->numVertices; i++)
+	{
+		if (color[i] == UNCOLORED)
+		{
+			front = rear = -1;
+			color[i] = 0;
+			enqueue(i);
+
+			while (!isEmpty())
+			{
+				int u = dequeue();
+				Node *temp = graph->adjLists[u];
+				while (temp)
+				{
+					int v = temp->vertex;
+					if (color[v] == UNCOLORED)
+					{
+						color[v] = 1 - color[u];
+						enqueue(v);
+					}
+					else if (color[v] == color[u])
+					{
+						return 0;
+					}
+					temp = temp->next;
+				}
+			}
+		}
+	}
+	return 1;
+}
